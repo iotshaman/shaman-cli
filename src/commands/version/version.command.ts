7bf119cdef1443e1c9864710f55a24419fbc3532
Mason Kleinsorge <kleinsorgem97@gmail.com>
2022-02-04 16:16:47 -0600

    add version command

--- src/commands/version/version.command.ts
+++ src/commands/version/version.command.ts
@@ -1,13 +1,12 @@
 import * as __path from "path";
 import { ICommand } from "..";
-import { Solution } from "../../models/solution";
 import { IFileService, FileService } from "../../services/file.service";
 
 export class VersionCommand implements ICommand {
     get name(): string { return "--version"; }
 
     fileservice: IFileService = new FileService();
-    
+
     run = (): Promise<void> => {
         let path = __path.join(__dirname, '../../..', 'package.json');
         return this.fileservice.readJson<{version: string}>(path).then(rslt => {
