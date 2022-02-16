2662e74798ca75f9adc400ce6e66531bb0f8a9b4
Mason Kleinsorge <kleinsorgem97@gmail.com>
2022-02-16 15:00:46 -0600

    re-order statements

--- src/commands/publish/dotnet/dotnet-publish.command.ts
+++ src/commands/publish/dotnet/dotnet-publish.command.ts
@@ -10,26 +10,29 @@ export class DotnetPublishCommand implements ICommand {
     get name(): string { return "publish-dotnet" };
     fileService = new FileService();
     environmentService: IEnvironmentService = new DotnetEnvironmentService();
-    
+
     run = (_environment: string, solutionFilePath: string): Promise<void> => {
-        let binPath = solutionFilePath.replace('shaman.json', 'bin/');
-        this.fileService.pathExists(`${binPath}dotnet`)
-            .then(exists => { if (!exists) this.fileService.createFolder(binPath, 'dotnet')});
         if (!solutionFilePath) solutionFilePath = _path.join(process.cwd(), 'shaman.json');
         else solutionFilePath = _path.join(process.cwd(), solutionFilePath);
-        console.log(`Publishing dotnet solutions...`);     
+        console.log(`Publishing dotnet solutions...`);
         return this.fileService.getShamanFile(solutionFilePath)
             .then(solution => this.publishSolution(solutionFilePath, solution));
     }
-    
-    private publishSolution(solutionFilePath: string, solution: Solution): Promise<void> {            
+
+    private publishSolution(solutionFilePath: string, solution: Solution): Promise<void> {
+        // NOTE: create /bin/dotnet directory
+        let binPath = solutionFilePath.replace('shaman.json', 'bin/');
+        this.fileService.pathExists(`${binPath}dotnet`)
+            .then(exists => { if (!exists) this.fileService.createFolder(binPath, 'dotnet') }); 
+        // TODO: remove if not needed
         if (!solution.projects.some(p => p.environment == "dotnet")) {
             console.log("No dotnet project detected, skipping dotnet build.");
             return Promise.resolve();
-          } 
-          let solutionFolderPath = solutionFilePath.replace('shaman.json', '');
-          let destinationPath = solutionFilePath.replace('shaman.json', 'bin/dotnet');
-          return this.environmentService.publishProject(solution.name, solutionFolderPath, destinationPath);          
+        }
+        //
+        let solutionFolderPath = solutionFilePath.replace('shaman.json', '');
+        let destinationPath = solutionFilePath.replace('shaman.json', 'bin/dotnet');
+        return this.environmentService.publishProject(solution.name, solutionFolderPath, destinationPath);
     }
 
 }
--- src/commands/publish/publish.command.ts
+++ src/commands/publish/publish.command.ts
@@ -14,24 +14,27 @@ export class PublishCommand implements ICommand {
     fileService: IFileService = new FileService();
     
     run = (environment: string = "*", solutionFilePath: string = "./shaman.json"): Promise<void> => {
-        if (environment != "*") { /* TODO: implement */ } 
+        // FIXME: get error if running specific environtment and wrong shaman file path
+        if (environment != "*") return this.publishEnvironment(environment, solutionFilePath);
         let publishEnvironmentsTask = this.fileService.getShamanFile(solutionFilePath).then(solution => {
             let projectEnvironments = solution.projects.map(p => p.environment);
             let environmetsSet = [...new Set(projectEnvironments)];
-            let cwd = solutionFilePath.replace("shaman.json", "");                        
-            return this.fileService.pathExists(`${cwd}/bin`)
-            .then(exists => { if (!exists) this.fileService.createFolder(cwd, "bin") })
-            .then(_ =>  environmetsSet.reduce((a, b) => a.then(_ => 
+            return environmetsSet.reduce((a, b) => a.then(_ => 
                 this.publishEnvironment(b, solutionFilePath)
-            ), Promise.resolve()));                           
+            ), Promise.resolve());                           
         });
         return publishEnvironmentsTask.then(_ => console.log("Solution publish is complete."));
     }
     
     private publishEnvironment(environment: string, solutionFilePath: string): Promise<void> {        
-        let cmd = this.publishCommands.find(c => c.name == `publish-${environment}`);
+        let cmd = this.publishCommands.find(c => c.name == `publish-${environment}`)
         if (!cmd) return Promise.reject(new Error(`Invalid environment '${environment}'.`));
+        let cwd = solutionFilePath.replace("shaman.json", "");  
+        // NOTE: create bin directory 
+        this.fileService.pathExists(`${cwd}/bin`) 
+        .then(exists => { if (!exists) this.fileService.createFolder(cwd, "bin") });
         return cmd.run(environment, solutionFilePath);
-    }    
+    }
+}    
 
-}
+
