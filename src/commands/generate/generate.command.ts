7fb9c55b16322bf660d3b5dbc0a3619e91062bb1
Mason Kleinsorge <Kleinsorgem97@outlook.com>
2022-10-06 17:09:02 -0500

    minor refactor

--- src/commands/generate/generate.command.ts
+++ src/commands/generate/generate.command.ts
@@ -8,10 +8,10 @@ import { ScaffoldCommand } from '../scaffold/scaffold.command';
 
 export class GenerateCommand implements ICommand {
     get name(): string { return "generate"; }
-    private fileService: IFileService = new FileService();
-    private recipeService: IRecipeService = new RecipeService();
+    fileService: IFileService = new FileService();
+    recipeService: IRecipeService = new RecipeService();
+    scaffoldCommand: ICommand = new ScaffoldCommand();
 
-    // shaman generate --name=NAME [--filePath=FILEPATH] [--recipe=RECIPE]
     private solutionName: string;
     private filePath: string;
     private recipeName: string;
@@ -20,29 +20,28 @@ export class GenerateCommand implements ICommand {
         this.assignArguments(cla);
         if (!this.solutionName) return Promise.reject(new Error('Name argument not provided to generate command.'));
         return this.fileService.pathExists(this.filePath)
-            .then(exists => { if (exists) throw new Error(`shaman.json file already exists at '${this.filePath}`); })
+            .then(exists => { if (exists) throw new Error(`shaman.json file already exists at '${this.filePath}'`); })
             .then(_ => this.recipeService.getRecipe(this.recipeName))
-            .then(recipe => this.createShamanFile(recipe))
+            .then(recipe => this.generateShamanFile(recipe))
             .then(_ => this.scaffoldSolution());
     };
 
-    private createShamanFile = (recipe: Recipe): Promise<void> => {
-        console.log('Creating shaman.json file...');
+    private generateShamanFile = (recipe: Recipe): Promise<void> => {
+        console.log('Generating shaman.json file...');
         let solution: Solution = {
             name: this.solutionName,
             projects: recipe.projects,
             transform: recipe.transform
         }
         return this.fileService.writeJson(this.filePath, solution)
-            .then(_ => console.log('Created shaman.json file.'));
+            .then(_ => console.log('Generated shaman.json file.'));
     }
 
     private scaffoldSolution = (): Promise<void> => {
         let scaffoldArguments = new CommandLineArguments(
             ['', '', 'scaffold', `--filePath=${this.filePath}`]
-        );
-        let scaffoldCommand = new ScaffoldCommand();
-        return scaffoldCommand.run(scaffoldArguments);
+        );        
+        return this.scaffoldCommand.run(scaffoldArguments);
     }
 
     assignArguments = (cla: CommandLineArguments) => {
--- src/services/recipe.service.ts
+++ src/services/recipe.service.ts
@@ -8,13 +8,13 @@ export interface IRecipeService {
 
 export class RecipeService implements IRecipeService {
 
-    private fileService: IFileService = new FileService();
-    private recipesFolder: string[] = [__dirname, '..', '..', 'recipes'];
+    fileService: IFileService = new FileService();
+    recipesFolder: string[] = [__dirname, '..', '..', 'recipes'];
 
     getRecipe = (recipeName: string): Promise<Recipe> => {
         let path = _path.join(...this.recipesFolder, `${recipeName}.json`);
         return this.fileService.pathExists(path)
-            .then(rslt => { if (!rslt) return Promise.reject(new Error(`Recipe not found: ${recipeName}`)); })
+            .then(rslt => { if (!rslt) return Promise.reject(new Error(`Recipe not found: '${recipeName}'`)); })
             .then(_ => this.fileService.readJson<{ recipe: Recipe }>(path))
             .then(recipe => recipe.recipe);
     };
